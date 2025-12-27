526719f501635cc6b0d98ec2348673597299b7d9
Mahta Zare <mahtazare77@gmail.com>
2025-12-27 15:12:42 -0600

    modified default security group

--- main.tf
+++ main.tf
@@ -44,8 +44,7 @@ resource "aws_default_route_table" "main-rtb" {
   }
 }
 
-resource "aws_security_group" "myapp-sg" {
-  name = "myapp-sg"
+resource "aws_default_security_group" "default-sg" {
   vpc_id = aws_vpc.myapp-vpc.id 
 
   ingress {
@@ -71,6 +70,6 @@ resource "aws_security_group" "myapp-sg" {
   }
 
   tags = {
-    Name: "${var.env_prefix}-sg"
+    Name: "${var.env_prefix}-default-sg"
   }
 }
