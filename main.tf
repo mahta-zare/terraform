917e67d5ec2a57daa0f218ddaa3fcbba08b75a26
Mahta Zare <mahtazare77@gmail.com>
2025-12-27 17:10:04 -0600

    added public key configurations

--- main.tf
+++ main.tf
@@ -6,6 +6,7 @@ variable "avail_zone" {}
 variable "env_prefix" {}
 variable "my_ip" {}
 variable "instance_type" {}
+variable "public_key_location" {}
 
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cidr_block
@@ -93,6 +94,11 @@ output "aws_ami_id" {
   value = data.aws_ami.latest-amazon-linux-image.id
 }
 
+resource "aws_key_pair" "ssh-key" {
+  key_name = "server-key"
+  public_key = file(var.public_key_location)
+}
+
 resource "aws_instance" "myapp-server" {
   ami = data.aws_ami.latest-amazon-linux-image.id
   instance_type = var.instance_type
@@ -102,7 +108,7 @@ resource "aws_instance" "myapp-server" {
   availability_zone = var.avail_zone
 
   associate_public_ip_address = true
-  key_name = "server-key-pair"
+  key_name = aws_key_pair.ssh-key.key_name
 
 
   tags = {
