f159f8b981042759d272a8ce13fcf855dfc0c936
Mahta Zare <mahtazare77@gmail.com>
2026-01-28 13:06:14 -0600

    exctracted outputs and variables from main.tf

--- main.tf
+++ main.tf
@@ -2,15 +2,6 @@ provider "aws" {
   region = "ca-west-1"
 }
 
-variable "vpc_cidr_block" {}
-variable "subnet_cidr_block" {}
-variable "avail_zone" {}
-variable "env_prefix" {}
-variable "my_ip" {}
-variable "instance_type" {}
-variable "public_key_location" {}
-variable "private_key_location" {}
-
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cidr_block
   tags = {
@@ -93,14 +84,6 @@ data "aws_ami" "latest-amazon-linux-image" {
     name = "virtualization-type"
     values = ["hvm"]
   }
-}
-
-output "aws_ami_id" {
-  value = data.aws_ami.latest-amazon-linux-image.id
-}
-
-output "ec2_public_ip" {
-  value = aws_instance.myapp-server.public_ip
 }
 
 resource "aws_key_pair" "ssh-key" {
